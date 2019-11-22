fcc0cb13c68c59cae7a7a024c143cb55ba820492
sugoidesune <timar003@gmail.com>
2019-11-22 01:32:35 +0100

    dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,18 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Readium' || process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700&display=swap' },
+    ],
+    script: [
+      {src: 'https://kit.fontawesome.com/5b47744dec.js', crossorigin:'anonymous'}
     ]
   },
   /*
---
+++ pages/tech/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+      love
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
